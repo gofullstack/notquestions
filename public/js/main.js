df6c94e10230b4bd6d37c1659a91afb9260e4681
Nathan L Smith <nlloyds@gmail.com>
2010-05-22 17:34:27 -0500

    public updates

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+    var learnMore = $(".learn_more");
+
+    learnMore.hide();
+    $("#learn_more_button").css({ display: "block" }).click(function () {
+        learnMore.slideToggle();
+        $(this).toggleClass("less");
+        return false;
+    });
+});
